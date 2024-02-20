42f0c38d989410f720cebf44dab35ee4c7634d9c
Akash G B <01fe22bca155@kletech.ac.in>
2024-02-20 09:48:51 +0530

    Extended Code

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@
  * The Calculator class performs basic arithmetic operations such as addition, subtraction,
  * multiplication, and division on two float operands.
  */
-class Calculator{
+class Calculator extends ExtendedCalculator{
 
     /**
      * The function "addition" takes two float operands and returns their sum.
@@ -62,5 +62,10 @@ class Calculator{
         System.out.println("Result of Multiplication is: " + mul_result);
         float div_result = calc.division(operand_1, operand_2);
         System.out.println("Result of Division is: " + div_result);
+        float square_result=calc.square(operand_1);
+        System.out.println("Result of Square is: " + square_result);
+        float cube_result=calc.cube(operand_1);
+        System.out.println("Result of Cube  is: " + cube_result);
+        
     }
 }
---
+++ ExtendedCalculator.java
@@ -0,0 +1,10 @@
+public class ExtendedCalculator {
+     float square(float operand_1){
+        return operand_1*operand_1;
+     }
+
+     float cube (float operand_1){
+        return operand_1*operand_1*operand_1;
+     }
+    
+}
